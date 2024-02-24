953015de7c44b9061a57d4d0c562d381ec5ea40e
Boris027 <titankiller027@gmail.com>
2024-02-24 13:12:33 +0100

    subida lista

---
+++ js/listado.js
@@ -0,0 +1,22 @@
+/****************************************/
+/*     Js: CRUD                         */
+/*     Made by: Boris Gallego Ríos      */
+/*     Versión 1.0                      */
+/****************************************/
+
+/*Chequea que se rellene correctamente el formulario */
+
+
+function modificar(){
+    alert('vas a modificar el usuario');
+}
+
+function eliminar(){
+    alert('vas a eliminar el usuario');
+}
+
+function insertar(){
+    alert('se van a insertar los datos')
+}
+
+
